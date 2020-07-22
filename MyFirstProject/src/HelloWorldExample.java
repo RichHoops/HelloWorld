9a5a41fa37d07560ec980d0e8eebf03e61b029cb
RichHoops <RichardHooper@myop.ac.nz>
2020-07-22 19:16:29 +1200

    New branch updating stuff.

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// Chnaging comments so w can see updates yolo
 		System.out.println("Hello World");
 	}
 
